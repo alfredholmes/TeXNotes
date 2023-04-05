4269c5f44b8dbc581233ac37a2c50dcdbe994dd8
Alfred <holmesey55@gmail.com>
2023-04-05 23:04:35 +0100

    note introspection and tags

--- LatexZettle/database.py
+++ LatexZettle/database.py
@@ -8,26 +8,38 @@ class BaseModel(pw.Model):
         database = database
 
 class Note(BaseModel):
-    filename = pw.CharField(max_length=200)  #filename of the note, eg note.tex or subfolder/note.tex
-    reference = pw.CharField(max_length=200) #\externaldocument reference for the note
+    filename = pw.CharField()  #filename of the note, eg note.tex or subfolder/note.tex
+    reference = pw.CharField() #\externaldocument reference for the note, default value for example_note.tex would be ExampleNote.
     last_build_date = pw.DateTimeField()
     last_edit_date = pw.DateTimeField()
 
 
-class Reference(BaseModel):
+class Citation(BaseModel):
     """
         Model to keep track of which notes reference papers.
     """
-    note = pw.ForeignKeyField(Note, on_delete='CASCADE')
-    citationkey = pw.CharField(max_length=200)
+    note = pw.ForeignKeyField(Note, on_delete='CASCADE', backref='citations')
+    citationkey = pw.CharField()
+
+
 
 class Link(BaseModel):
     """
         Model to keep track of links between files
     """
-    source = pw.ForeignKeyField(Note, on_delete='CASCADE') #note containing the reference
-    target = pw.ForeignKeyField(Note, on_delete='CASCADE') #note being referenced
-    target_label = pw.CharField(max_length=200) #\excref{target}{target_label}
+    source = pw.ForeignKeyField(Note, on_delete='CASCADE', backref='references') #note containing the reference
+    target = pw.ForeignKeyField(Note, on_delete='CASCADE', backref='referenced_by') #note being referenced
+    target_label = pw.CharField() #\excref{target}{target_label}
+
+class Label(BaseModel):
+    note = pw.ForeignKeyField(Note, on_delete='CASCADE', backref='labels')
+    label = pw.CharField()
+
+class Tag(BaseModel):
+    name = pw.CharField()
+    notes = pw.ManyToManyField(Note)
+
+NoteTag = Tag.notes.get_through_model()
 
 def create_tables(*models):
     with database:
--- manage.py
+++ manage.py
@@ -2,54 +2,68 @@
 import sys
 
 import shutil
-from LatexZettle import files
+from LatexZettle import files, database
+import re
 import os
 
 class Helper:
 
-
     def help():
         print("""
 
-            Manage the LaTeX slip box.
-
-
+            Manage the LaTeX slip box. This file should never be executed with the working directory not the base directory of the slipbox project.
         """)
 
-    def newnote(note_name, reference_name=None):
+    def addtodocuments(filename, reference=""):
+        """
+            Adds line \externaldocument[reference-]{filename} to documents.tex
+            If reference is not supplied then it defaults to, for example, NoteName if filename=note_name
+        """
+        if reference == "":
+            reference = ''.join([w.capitalize() for w in filename.split('_')])
+
+        with open('documents.tex', 'a') as f:
+            f.write(f'\externaldocument[{reference}-]{{{filename}}}\n')
+
+    def getnotefiles(directory='notes'):
+        notes = [str(f) for f in files.get_files(directory)]
+        return notes
+
+    def createnotefile(filename):
+        shutil.copyfile('template/note.tex', f'notes/{filename}.tex')
+    
+
+    def newnote(note_name, reference_name=""):
         """
             createnote note_name [Optional ReferenceName]
             Creates note with name note_name.tex, Second argument is optional and is the name in the reference, defaults to NoteName
 
+            to do: check whether a file already exists etc
+
         """
-
-        if reference_name is None:
-            reference_name = ''.join([w.capitalize() for w in note_name.split('_')])
-
     
-        shutil.copyfile('template/note.tex', f'notes/{note_name}.tex')
-
-        with open('documents.tex', 'a') as f:
-            f.write(f'\externaldocument[{reference_name}-]{{{note_name}}}\n')
-
+        Helper.createnotefile(note_name)
+        Helper.addtodocuments(note_name, reference_name)
 
     def renderallhtml():
         """
             Renderes all the notes using make4ht. Saves output in /html
         """
 
-        notes = [str(f) for f in files.get_files('notes')]
+        notes = Helper.getnotefiles()
+        try:
+            os.mkdir('html')
+        except FileExistsError:
+            pass
         os.chdir('html')
 
         for note in notes:
-            filename = 'notes/'.join(note.split('notes/')[1:])
-            filename = filename[:-4] 
+            filename = note[:-4] 
             os.system(f'make4ht ../{note} svg')
             os.system(f'biber {filename}')
 
         for note in notes:
-            filename = 'notes/'.join(note.split('notes/')[1:])
-            filename = filename[:-4] 
+            filename = note[:-4] 
             os.system(f'make4ht ../{note} svg')
        
        
@@ -58,19 +72,112 @@ class Helper:
             Renderes all the notes using pdflatex. Saves output in /pdf
         """
         
-        notes = [str(f) for f in files.get_files('notes')]
+        notes = Helper.getnotefiles()
+        try:
+            os.mkdir('pdf')
+        except FileExistsError:
+            pass
         os.chdir('pdf')
 
         for note in notes:
-            filename = 'notes/'.join(note.split('notes/')[1:])
-            filename = filename[:-4] 
+            filename = note[:-4] 
             os.system(f'pdflatex ../{note} svg')
             os.system(f'biber {filename}')
 
         for note in notes:
-            filename = 'notes/'.join(note.split('notes/')[1:])
-            filename = filename[:-4] 
+            filename = note[:-4] 
             os.system(f'pdflatex ../{note} svg')
+
+
+    def getyesno():
+        while True:
+            a = input()
+            if a == 'y':
+                return True
+            elif a == 'n':
+                return False
+            else:
+                print('Please enter either \'y\' or \'n\'')
+
+    def synchronize():
+        """
+            Reads the file documents.tex and adds these files to the database, then checks for files in /notes that aren't in the documents
+        """
+
+        notes = ['notes/'.join(str(f).split('notes/')[1:])[:-4] for f in files.get_files('notes')]
+        #get all the tracked notes
+        tracked_notes = {}
+        with open('documents.tex', 'r') as f:
+            for line in f:
+                m = re.search('(\\\\externaldocument\[)(.+?)(\-\]\{)(.+?)(\})', line)
+                if m:
+                    reference_name = m.group(2)
+                    filename = m.group(4)
+
+                    if filename not in notes:
+                        print(f'File {filename} with reference {reference_name} missing from notes. Make new note now? (y/n)')
+                        if Helper.getyesno():
+                            Helper.createnotefile(filename)
+                            tracked_notes[filename] = reference_name
+                            
+                    else:
+                        tracked_notes[filename] = reference_name
+
+        for note in notes:
+            if note not in tracked_notes:
+                print(f'File {note} not tracked by the file documents.tex. Add to the file now? (y/n)')
+                if Helper.getyesno(): 
+                    reference = ''.join([w.capitalize() for w in note.split('_')])
+                    print(f'Reference (defaults to {reference}):', end='')
+                    reference = input()
+                    Helper.addtodocuments(note, reference)
+
+
+
+
+
+    def getlabels():
+        notes = Helper.getnotefiles()
+        for note in notes:
+            with open(f'{note}') as f:
+                for line in f:
+                    labels = re.search('(\\\\label\{)(.*?)(\})', line)
+                    try:
+                        label = labels.group(2)
+                        print(label)
+                    except AttributeError:
+                        pass
+
+
+
+    def getlinks():
+        notes = Helper.getnotefiles()
+        for note in notes:
+            with open(note) as f:
+                for line in f:
+                    links = re.finditer('\\\\ex(hyper)?(c)?ref\{(.*?)\}\{(.*?)\}', line)
+
+                    for link in links:
+                        print(f'file: {link.group(3)}, label referenced {link.group(4)}')
+
+    def gettags():
+        notes = Helper.getnotefiles()
+        tags = {}
+        for note in notes:
+            with open(note) as f:
+                lines = f.read().splitlines()
+                last_line = lines[-1]
+                if re.search("\\\\end\{document\}", last_line) is None:
+                    note_tags = [f.lower() for f in last_line.strip().split(",")]
+                    for tag in note_tags:
+                        print(note)
+                        tags[tag] = ('notes/'.join(note.split('notes/')[1:]))[:-4]
+
+
+        print(tags)
+
+
+                        
 
 
 
